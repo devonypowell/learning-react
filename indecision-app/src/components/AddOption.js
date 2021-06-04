b024ca9d9c5bc2025604a060b6b540b2b9889db5
Devony Powell <pdevony93@gmail.com>
2021-06-03 23:17:10 -0500

    apply styles to components elements

--- indecision-app/src/components/AddOption.js
+++ indecision-app/src/components/AddOption.js
@@ -1,33 +1,33 @@
 import React from 'react';
 
 export default class AddOption extends React.Component {
-    state = {
-        error: undefined
-    };
-    
-    handleAddOption = (e) => {        
-        e.preventDefault();
-        const option = e.target.elements.option.value.trim();
-        const error = this.props.handleAddOption(option);
+  state = {
+    error: undefined,
+  };
 
-        // es6 syntax; this is equivalent to => return {error: error}
-        this.setState(() => ({ error }));
+  handleAddOption = (e) => {
+    e.preventDefault();
+    const option = e.target.elements.option.value.trim();
+    const error = this.props.handleAddOption(option);
 
-        // wipe input if there is no error
-        if(!error) {
-            e.target.elements.option.value = '';
-        }
+    // es6 syntax; this is equivalent to => return {error: error}
+    this.setState(() => ({ error }));
+
+    // wipe input if there is no error
+    if (!error) {
+      e.target.elements.option.value = '';
     }
+  };
 
-    render() {
-        return (
-            <div>
-                {this.state.error && <p>{this.state.error}</p>}
-                <form onSubmit={this.handleAddOption}>
-                    <input type="text" name="option"/>
-                    <button>Add Option</button>
-                </form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option" />
+          <button className="button">Add Option</button>
+        </form>
+      </div>
+    );
+  }
 }
